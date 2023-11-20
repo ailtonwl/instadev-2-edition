ff35ce39b0e0430ddf4d1ea889e4a0d0ed762c0b
Ailton Martins Guimarães <ailton@workline.com.br>
2023-11-20 15:59:52 -0300

    Aula 8 - Create bottom menu

---
+++ src/components/BottomBar/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="full-width row justify-around">
+    <q-img src="../../assets/home.svg" width="26px"></q-img>
+    <q-img src="../../assets/search.svg" width="28px"></q-img>
+    <q-img src="../../assets/add.png" width="28px"></q-img>
+    <q-img src="../../assets/heart.svg" width="28px"></q-img>
+    <q-avatar size="28px">
+      <img src="https://cdn.quasar.dev/img/avatar.png">
+    </q-avatar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BottomBar',
+};
+</script>
+
+<style lang="scss" scoped>
+.q-img {
+  cursor: pointer;
+}
+</style>
--- src/components/TopBar/Index.vue
+++ src/components/TopBar/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="full-width row justify-between">
-    <q-img class="q-ml-sm camera" src="../../assets/camera.svg" width="24px"></q-img>
+    <q-img class="q-ml-sm camera" src="../../assets/camera.svg" width="28px"></q-img>
     <q-img class="logo" src="../../assets/instadev-logo.svg"></q-img>
     <div>
-      <q-img class="q-mr-lg" src="../../assets/reels.svg" width="28px"></q-img>
-      <q-img class="q-mr-sm" src="../../assets/direct.svg" width="28px"></q-img>
+      <q-img class="q-mr-lg" src="../../assets/igtv.svg" width="28px"></q-img>
+      <q-img class="q-mr-sm" src="../../assets/gpswl.png" width="28px"></q-img>
     </div>
   </div>
 </template>
@@ -17,7 +17,7 @@ export default {
 
 <style lang="scss" scoped>
 .logo {
-  width: 105px;
+  width: 109px;
   height: 28px;
 }
 .camera {
